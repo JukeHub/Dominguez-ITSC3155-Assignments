74bbe4a6e3f19214487b8c6603b06d557d191599
Julian Dominguez <jdominguez9@gmail.com>
2026-02-21 13:14:26 -0500

    feat: Implement existing main method from my Assignment1 and add gitignore

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -5,14 +5,34 @@ from cashier import Cashier
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = #####
-cashier_instance = ######
-
-
-
+sandwich_maker_instance = SandwichMaker(resources)
+cashier_instance = Cashier()
 
 def main():
-    ###  write the rest of the codes ###
+    is_on = True
+
+    while is_on:
+
+        order = input("What would you like? (small / medium / large / off / report): ").lower()
+
+        if order == "report":
+            for key, value in sandwich_maker_instance.machine_resources.items():
+                if key == "bread" or key == "ham":
+                    print(f"{key.capitalize()}: {value} slice(s)")
+                else:
+                    print(f"{key.capitalize()}: {value} ounces(s)")
+        elif order in recipes:
+            ingredients = recipes[order]["ingredients"]
+            cost = recipes[order]["cost"]
+            if sandwich_maker_instance.check_resources(ingredients):
+                payment = cashier_instance.process_coins()
+                if cashier_instance.transaction_result(payment, cost):
+                    sandwich_maker_instance.make_sandwich(order, ingredients)
+        elif order == "off":
+            is_on = False
+            print("Turning machine off!")
+        else:
+            print("Invalid entry, please review our options and try again.")
 
 if __name__=="__main__":
     main()
